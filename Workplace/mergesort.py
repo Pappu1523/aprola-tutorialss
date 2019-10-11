61e066eb8bf81ed4617d6914de69debe499d7c3f
pappu1523 <you@example.com>
2019-10-11 19:35:01 +0530

    mergesort pgm

--- Workplace/mergesort.py
+++ Workplace/mergesort.py
@@ -1,33 +1,33 @@
-def mergeSort(nlist):
-    print("Splitting ",nlist)
-    if len(nlist)>1:
-        mid = len(nlist)//2
-        lefthalf = nlist[:mid]
-        righthalf = nlist[mid:]
+def mergeSort(list):
+   
+    if len(list)>1:
+        mid = len(list)//2
+        lefthalf = list[:mid]
+        righthalf = list[mid:]
 
         mergeSort(lefthalf)
         mergeSort(righthalf)
         i=j=k=0       
         while i < len(lefthalf) and j < len(righthalf):
             if lefthalf[i] < righthalf[j]:
-                nlist[k]=lefthalf[i]
+                list[k]=lefthalf[i]
                 i=i+1
             else:
-                nlist[k]=righthalf[j]
+                list[k]=righthalf[j]
                 j=j+1
             k=k+1
 
         while i < len(lefthalf):
-            nlist[k]=lefthalf[i]
+            list[k]=lefthalf[i]
             i=i+1
             k=k+1
 
         while j < len(righthalf):
-            nlist[k]=righthalf[j]
+            list[k]=righthalf[j]
             j=j+1
             k=k+1
-    print("Merging ",nlist)
+    print("Merging ",list)
 
-nlist = [14,46,43,27,57,41,45,21,70]
-mergeSort(nlist)
-print(nlist)
+list = [14,46,43,27,57,41,45,21,70]
+mergeSort(list)
+print(list)
